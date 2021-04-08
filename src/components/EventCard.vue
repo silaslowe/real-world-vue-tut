0b6a03c67ed606405bec40d5a79826ca8b6933d6
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-04-08 13:12:58 -0500

    lesson 3 dun

---
+++ src/components/EventCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="event-card">
+    <span>{{ event.time }} on {{ event.date }}</span>
+    <h4>{{ event.title }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EventCard",
+  props: {
+    event: {
+      event: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
